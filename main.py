4029c475cc47309b9dced7df5bc471147bd775bd
tomas <tomas.f.ribeiro@sapo.pt>
2022-05-26 20:57:01 +0100

    implementation done

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ class Vertex:
     def __hash__(self):
         return hash(id(self))
 
+    def __str__(self):
+        return f"${str(self.__element)}$"
+    def __repr__(self):
+        return str(self)
 class Edge:
     def __init__(self, Vertex1 : Vertex, Vertex2 : Vertex, peso : Real, isDirected = False):
         self.aresta : Tuple[Vertex, Vertex] = (Vertex1, Vertex2)
@@ -23,6 +27,11 @@ class Edge:
         self.isDirected = isDirected
     def __getitem__(self, item):
         return self.aresta[item]
+
+    def __str__(self):
+        return f"%{str(self.aresta[0])} {('->' if self.isDirected else '<->')} {str(self.aresta[1])} ; peso = {self.peso}%"
+    def __repr__(self):
+        return str(self)
 class Graph:
     def __init__(self, isDirected = False):
         self._map : Dict[Vertex, Dict[Vertex, Edge]] = dict()
@@ -48,16 +57,36 @@ class Graph:
         if not self.isDirected: raise AttributeError("Graph is not directed")
         return sum(b for a, b in self._map[v] if b[0] is v)
     def insert_vertex(self, v : Vertex):
+        if not isinstance(v, Vertex):
+            v = Vertex(v)
+        self._insert_vertex(v)
+        return v
+    def _insert_vertex(self, v: Vertex):
         self._vertices.add(v)
         self._map[v] = dict()
-
-    def insert_edge(self, e : Edge):
+    def _insert_edge(self, e : Edge):
         if self.isDirected == e.isDirected:
             self._map[e.aresta[0]][e.aresta[1]] = e
             self._map[e.aresta[1]][e.aresta[0]] = e
             self._edges.add(e)
+            return e
         else:
             raise AttributeError(f"Edge is{' not ' if not e.isDirected else ''}directed")
+    def insert_edge(self, e : Edge, *args):
+        if not isinstance(e, Edge):
+            if not isinstance(e, tuple):
+                try:
+                    e = Edge(e, args[0], args[1])
+                except:
+                    raise TypeError
+            else:
+                try:
+                    e = Edge(*e)
+                except:
+                    raise TypeError
+        self._insert_edge(e)
+        return e
+
 
     def remove_edge(self, e : Edge):
         del self._map[e.aresta[0]][e.aresta[1]]
@@ -67,3 +96,30 @@ class Graph:
     def remove_vertex(self, v : Vertex):
         del self._map[v]
         self._vertices.remove(v)
+    def __str__(self):
+        return str(self._map)
+if __name__ == "__main__":
+    g = Graph()
+    v1 = g.insert_vertex(13)
+    v2=g.insert_vertex(11)
+    v3=g.insert_vertex(10)
+    '''
+    g.remove_vertex(v1)
+    '''
+    e1 = g.insert_edge(Edge(v1, v2, 10))
+    e2 = g.insert_edge(Edge(v1, v3, 90))
+    e3 = g.insert_edge(Edge(v2, v3, 67))
+    '''
+    g.remove_edge(e1)
+    '''
+    print(g.vertex_count())
+    print(g.vertices())
+    print(g.edges_count())
+    print(g.edges())
+    print(g.get_edge(v1, v2))
+    print(g.degree(v1))
+    '''
+    print(g.outdegree(v1))
+    print(g.indegree(v1))
+    '''
+    print(g)
